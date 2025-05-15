7c4c5eca082ba5e4b3517c87029d2c2198e76069
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 09:36:49 -0500

    :sparkles: Feat: Configure dependency bindings in InfrastructureModule

--- app/infrastructure/infrastructure_module.py
+++ app/infrastructure/infrastructure_module.py
@@ -1,6 +1,35 @@
 from injector import Module, inject, singleton
+from app.ports.repositories import (
+    UserRepository,
+)
+from app.ports.logging import LoggerServicePort
+    
+from app.ports.services.hasher_service_port import (
+    HasherServicePort,
+    VerifyDataServicePort
+)
 
+from app.infrastructure.database.repositories.user_repository import (
+    SQLAlchemyUserRepository
+)
+from app.infrastructure.logging import (
+    ConsoleLoggerService,
+)
+
+from app.infrastructure.security.passlib_data_hasher import (
+    PasslibDataHasher,
+)
+
+from app.infrastructure.security.passlib_data_verifier import (
+    PasslibDataVerifier,
+)
 
 class InfrastructureModule(Module):
     def configure(self, binder):
-        pass
+
+        binder.bind(UserRepository, to=SQLAlchemyUserRepository)
+        binder.bind(LoggerServicePort, to=ConsoleLoggerService)
+        binder.bind(HasherServicePort, to=PasslibDataHasher)
+        binder.bind(VerifyDataServicePort, to=PasslibDataVerifier)
+
+
