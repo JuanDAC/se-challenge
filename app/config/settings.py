a0486a82fa8afd7b00ab88e28c1e33fff5de3641
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 03:25:04 -0500

    :sparkles: Feat: Add settings module

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,6 +4,12 @@ from functools import lru_cache
 class Settings:
     def __init__(self):
         self.API_V1_STR = "/api/v1"
+        self.crypt_context_settings = {
+            "schemes": ["bcrypt"],
+            "default": "bcrypt",
+            "deprecated": "auto",
+            "bcrypt__rounds": 13,
+        }
 
 
 @lru_cache
