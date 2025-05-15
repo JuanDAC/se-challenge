aaa3c2242fa583d5a2dbe997e0f257a10c1c0647
JuanDAC <juanavila12.0308@gmail.com>
2025-05-14 22:33:24 -0500

    :sparkles: Feat: Implement FastAPI application setup with CORS and users router

--- app/presentation/http/app.py
+++ app/presentation/http/app.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.presentation.http.routers.users import router as users_router
+from app.config.environment import get_environment_variables
+from app.config.settings import get_settings
 
+envs = get_environment_variables()
+settings = get_settings()
+
+app = FastAPI(
+    root_path=settings.API_V1_STR,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    title=envs.APP_NAME,
+    version=envs.API_VERSION,
+    swagger_ui_parameters={"syntaxHighlight": {"theme": "obsidian"}},
+)
+
+app.include_router(users_router, tags=["users"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
