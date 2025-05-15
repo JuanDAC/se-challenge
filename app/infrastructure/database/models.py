12361558aae1158d8da7984155200cbf0f562023
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 03:25:05 -0500

    :sparkles: Feat: Add SQLAlchemy User model

--- app/infrastructure/database/models.py
+++ app/infrastructure/database/models.py
@@ -1,1 +1,37 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum as SAEnum
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from datetime import timezone
+from datetime import datetime
+from app.domain.entities.users import UserRole
+from sqlalchemy.ext.declarative import declarative_base
+from app.domain.entities.users import UserInDBBase
 
+Base = declarative_base()
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    role = Column(SAEnum(UserRole), default=UserRole.USER, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+    deleted_at = Column(DateTime(timezone=True), nullable=True, index=True)
+    active = Column(Boolean, default=True)
+
+    def to_pydantic(self) -> UserInDBBase:
+        return UserInDBBase.model_validate(self)
