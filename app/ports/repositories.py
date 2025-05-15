199349c2748a557212f2f8eb9cd1ea6e600f7dee
JuanDAC <juanavila12.0308@gmail.com>
2025-05-14 21:56:14 -0500

    :file_folder: Port: Add UserRepository interface

--- app/ports/repositories.py
+++ app/ports/repositories.py
@@ -1,1 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from uuid import UUID
+from app.domain.entities.user import UserCreateSchema, UserUpdateSchema, UserResponseSchema, UserRole, UserInDBBaseSchema
 
+class UserRepository(ABC):
+    @abstractmethod
+    def get_by_id(self, user_id: UUID) -> Optional[UserInDBBaseSchema]:
+        pass
+
+    @abstractmethod
+    def get_by_username(self, username: str) -> Optional[UserInDBBaseSchema]:
+        pass
+
+    @abstractmethod
+    def get_by_email(self, email: str) -> Optional[UserInDBBaseSchema]:
+        pass
+
+    @abstractmethod
+    def get_all(self, skip: int = 0, limit: int = 100) -> List[UserInDBBaseSchema]:
+        pass
+
+    @abstractmethod
+    def add(self, user_data: UserCreateSchema) -> UserInDBBaseSchema:
+        pass
+
+    @abstractmethod
+    def update(self, user_id: UUID, user_data: UserUpdateSchema) -> Optional[UserInDBBaseSchema]:
+        pass
+
+    @abstractmethod
+    def delete(self, user_id: UUID) -> bool:
+        pass
