889d626bf800c2fb685bb706f4398d983fb4a960
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 03:25:08 -0500

    :file_folder: Port: Add UserRepository interface

--- app/ports/repositories.py
+++ app/ports/repositories.py
@@ -1,40 +1,36 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
 from uuid import UUID
-from app.domain.entities.user import (
-    UserCreateSchema,
-    UserUpdateSchema,
-    UserResponseSchema,
-    UserRole,
-    UserInDBBaseSchema,
+from app.domain.entities.users import (
+    UserCreate,
+    UserUpdate,
+    UserInDBBase,
 )
 
 
 class UserRepository(ABC):
     @abstractmethod
-    def get_by_id(self, user_id: UUID) -> Optional[UserInDBBaseSchema]:
+    def get_by_id(self, user_id: UUID) -> Optional[UserInDBBase]:
         pass
 
     @abstractmethod
-    def get_by_username(self, username: str) -> Optional[UserInDBBaseSchema]:
+    def get_by_username(self, username: str) -> Optional[UserInDBBase]:
         pass
 
     @abstractmethod
-    def get_by_email(self, email: str) -> Optional[UserInDBBaseSchema]:
+    def get_by_email(self, email: str) -> Optional[UserInDBBase]:
         pass
 
     @abstractmethod
-    def get_all(self, skip: int = 0, limit: int = 100) -> List[UserInDBBaseSchema]:
+    def get_all(self, skip: int = 0, limit: int = 100) -> List[UserInDBBase]:
         pass
 
     @abstractmethod
-    def add(self, user_data: UserCreateSchema) -> UserInDBBaseSchema:
+    def add(self, user_data: UserCreate) -> UserInDBBase:
         pass
 
     @abstractmethod
-    def update(
-        self, user_id: UUID, user_data: UserUpdateSchema
-    ) -> Optional[UserInDBBaseSchema]:
+    def update(self, user_id: UUID, user_data: UserUpdate) -> Optional[UserInDBBase]:
         pass
 
     @abstractmethod
