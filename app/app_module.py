73a945bc26d682d50e3a169904aabb7cb1139a50
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 19:56:24 -0500

    :recycle: Refactor: Install ConfigModule in AppModule

--- app/app_module.py
+++ app/app_module.py
@@ -1,12 +1,14 @@
 from injector import Injector, Module
 from app.infrastructure.infrastructure_module import InfrastructureModule
 from app.use_cases.use_cases_module import UseCasesModule
+from app.config.config_module import ConfigModule
 
 
 class AppModule(Module):
     def configure(self, binder):
         super().configure(binder)
 
+        binder.install(ConfigModule())
         binder.install(InfrastructureModule())
         binder.install(UseCasesModule())
 
