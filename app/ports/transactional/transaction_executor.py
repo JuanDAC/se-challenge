e994f4d708a9ef008d13dd7e3abaf52aaa533c12
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 19:56:34 -0500

    :file_folder: Port: Add transactional port directory

--- app/ports/transactional/transaction_executor.py
+++ app/ports/transactional/transaction_executor.py
@@ -1,6 +1,7 @@
 from typing import TypeVar, Generic, Iterator
 from abc import abstractmethod, ABC
 from contextlib import contextmanager
+from app.ports.logging import LoggerServicePort
 
 Session = TypeVar("Session")
 
@@ -8,11 +9,15 @@ Session = TypeVar("Session")
 class TransactionExecutor(Generic[Session]):
     session: Session = None
 
+    def __init__(self, logger: LoggerServicePort):
+        self.logger = logger
+
     def set_transaction_context(self, session: Session) -> None:
         self.session = session
 
     @contextmanager
     def get_session(self) -> Iterator[Session]:
         if not hasattr(self, "session") and self.session is None:
+            self.logger.error("Session is not set", exc_info=True)
             raise Exception("Session is not set")
         yield self.session
