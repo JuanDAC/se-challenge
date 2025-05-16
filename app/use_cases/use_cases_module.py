85e7beadf4cd92f58857a154522fc1159f0ffa9d
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 20:02:12 -0500

    :sparkles: Feat: Implement use cases module for user management

--- app/use_cases/use_cases_module.py
+++ app/use_cases/use_cases_module.py
@@ -12,6 +12,7 @@ from app.use_cases.user.get_user import GetUser, ListUsers
 from app.use_cases.user.update_user import UpdateUser
 from app.use_cases.user.delete_user import DeleteUser
 
+
 class UseCasesModule(Module):
     def configure(self, binder):
         super().configure(binder)
@@ -21,4 +22,3 @@ class UseCasesModule(Module):
         binder.bind(UpdateUserUseCase, to=UpdateUser)
         binder.bind(DeleteUserUseCase, to=DeleteUser)
         binder.bind(ListUsersUseCase, to=ListUsers)
-
