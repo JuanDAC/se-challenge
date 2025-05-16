59dc9f8803aafc6ee3a2538f328f80421906ff04
JuanDAC <juanavila12.0308@gmail.com>
2025-05-15 19:56:45 -0500

    :white_check_mark: Test: Add pytest fixtures and test client setup for FastAPI

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,43 +3,66 @@ from fastapi.testclient import TestClient
 from app.presentation.http.app import app as fastapi_app
 from tests.fixtures.in_memory_user_repository import InMemoryUserRepository
 from uuid import uuid4
-from injector import Injector, Module, provider, singleton
+from injector import Injector, Module, noscope
 from app.ports.repositories import UserRepository
 
 from app.config.config_module import ConfigModule
 from app.use_cases.use_cases_module import UseCasesModule
+from app.infrastructure.infrastructure_module import InfrastructureModule
+from app.ports.transactional.transaction_manager import TransactionManagerPort
+from contextlib import contextmanager   
+from typing import Iterable
 
 
-@pytest.fixture(scope="function")
 def in_memory_user_repo():
     repo = InMemoryUserRepository()
     yield repo
     repo.clear()
 
 
-@pytest.fixture(scope="function")
-def client(in_memory_user_repo_instance: InMemoryUserRepository):
-    class TestSpecificInfrastructureModule(Module):
-        def __init__(self, repo_instance: InMemoryUserRepository):
-            self._repo_instance = repo_instance
 
-        @singleton
-        @provider
-        def provide_user_repository(self) -> UserRepository:
-            return self._repo_instance
+class TransactionManager(TransactionManagerPort[bool]):
+    @contextmanager
+    def get_transaction_context(self) -> Iterable[bool]:
+        try:
+            yield True
+        except Exception as e:
+            raise e
+        finally:
+            pass
+
+
+
+@pytest.fixture(scope="function")
+def client():
+    class TestSpecificInfrastructureModule(Module):
+        def configure(self, binder):
+
+            binder.bind(UserRepository, to=InMemoryUserRepository, scope=noscope)
+            binder.bind(
+                TransactionManagerPort,
+                to=TransactionManager,
+                scope=noscope,
+            )
+
+    class AppModule(Module):
+        def configure(self, binder):
+            super().configure(binder)
+
+            binder.install(ConfigModule(exclude_classes=[TransactionManagerPort]))
+            binder.install(InfrastructureModule(exclude_classes=[UserRepository]))
+            binder.install(UseCasesModule())
+            binder.install(TestSpecificInfrastructureModule())
+
 
     test_injector = Injector(
-        [
-            ConfigModule(),
-            UseCasesModule(),
-            TestSpecificInfrastructureModule(in_memory_user_repo_instance),
-        ]
+        [AppModule]
     )
 
     try:
         import app.app_module as app_dependencies_module
 
-        app_dependencies_module.instance = test_injector
+        app_dependencies_module.injector = test_injector
 
     except (ImportError, AttributeError) as e:
         raise ImportError(
@@ -49,22 +72,7 @@ def client(in_memory_user_repo_instance: InMemoryUserRepository):
 
     with TestClient(fastapi_app) as test_client:
         yield test_client
-
-
-@pytest.fixture(scope="function")
-def client_functional(in_memory_user_repo):
-    from app.app_module import get_user_repository_dependency  # Make sure this exists
-
-    original_overrides = fastapi_app.dependency_overrides.copy()
-    fastapi_app.dependency_overrides[get_user_repository_dependency] = (
-        lambda: in_memory_user_repo
-    )
-
-    with TestClient(fastapi_app) as c:
-        yield c
-
-    fastapi_app.dependency_overrides = original_overrides
-
+        
 
 @pytest.fixture(scope="function")
 def unique_user_payload(faker):
